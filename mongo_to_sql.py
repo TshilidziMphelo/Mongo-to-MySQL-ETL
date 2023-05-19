1a104765a0da149d14c219902aa305c73490616d
Tshilidzi Emmanuel Mphelo <temphelo95@gmail.com>
2023-05-19 20:24:36 +0200

    Tried to read data into mysql but I keep getting document invalid cannot encode object

--- mongo_to_sql.py
+++ mongo_to_sql.py
@@ -8,4 +8,26 @@ data = collection.find({}, {'_id': 0})
 
 df = pd.DataFrame(list(data))
 
-print(f"Reading mongo data was successful")
+mysql_conn = mysql.connector.connect(
+    host="localhost",
+    user="eai_rw",
+    password="BidSt3yn100%",
+    database="gold_price_data"   # Your MySQL database name
+)
+
+cursor = mysql_conn.cursor()
+
+for _, row in df.iterrows():
+    insert_query = """
+    INSERT INTO imdb_highest_ratings (id, title, type, genres, averageRating, numVotes, releaseYear)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON DUPLICATE KEY UPDATE
+    title=VALUES(title),
+    type=VALUES(type),
+    genres=VALUES(genres),
+    averageRating=VALUES(averageRating),
+    numVotes=VALUES(numVotes),
+    releaseYear=VALUES(releaseYear)
+    """
+    cursor.execute(insert_query, tuple(row))
+
